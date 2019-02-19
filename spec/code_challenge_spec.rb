df3fd17b3078842c3c8d9f3f96c8f647f68038aa
otomatonom <compower4@gmail.com>
2019-02-19 14:41:05 -0800

    Test file issues

--- spec/code_challenge_spec.rb
+++ spec/code_challenge_spec.rb
@@ -3,7 +3,10 @@ RSpec.describe CodeChallenge do
     expect(CodeChallenge::VERSION).not_to be nil
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  it "should return [ok] if the user puts a key-value pair in the hash" do
+    CodeChallenge.main().stub(:gets) {"put stuff here"}
+    expect(CodeChallenge.main()).to eq(puts "ok\n")
+    CodeChallenge.main().stub(:gets) {"exit"}
+    expect(CodeChallenge.main()).to eq(puts "\nBye!\n\n")
   end
 end
